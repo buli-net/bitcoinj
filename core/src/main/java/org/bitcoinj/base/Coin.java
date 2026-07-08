873094e6785e4ef020f4084cc33f9b2b4cc5f895
buli <hvlrcb@gmail.com>
2026-07-08 21:05:12 +0700

    Update Coin.java

--- core/src/main/java/org/bitcoinj/base/Coin.java
+++ core/src/main/java/org/bitcoinj/base/Coin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Andreas Schildbach
+ * Copyright 2014 Andreas Schildbach 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
